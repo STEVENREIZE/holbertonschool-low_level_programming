61484b95189b6bb8fe9ad6226957c5e553579129
steven reize <stevenreize15@gmail.com>
2026-02-27 16:04:37 +0100

    tous les tasks

---
+++ variables_if_else_while/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - Computes the absolute value of an interger.
+ * @n: The interger to be computed.
+ *
+ * Return: The absolue value of the interger.
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
